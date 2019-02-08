a522ec9713c60b08d12cf11e7d981ddf1cf4bf83
syntacticNaCl <garrettrappaport@protonmail.ch>
2019-02-08 00:39:59 -0500

    Add docker-ce and docker-compose install

--- installs/linux.sh
+++ installs/linux.sh
@@ -19,8 +19,8 @@ echo "Building emacs dependencies..."
 sudo apt-get build-dep emacs
 
 echo "Building emacs..."
-# cd /tmp/ && git clone https://github.com/emacs-mirror/emacs.git
-# cd /tmp/emacs && ./autogen.sh && ./configure && make -j4 && sudo make install
+cd /tmp/ && git clone https://github.com/emacs-mirror/emacs.git
+cd /tmp/emacs && ./autogen.sh && ./configure && make -j4 && sudo make install
 echo "Emacs build finished!"
 
 echo "Installing spacemacs"
@@ -28,6 +28,29 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 ln -sf $dir/.spacemacs ~/
 ln -sf $dir/emacs-private ~/.emacs.d/private
+
+echo "Installing docker and docker-compose..."
+sudo apt update && sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+
+install_dockerce() {
+   echo "Installing docker-ce..."
+   sudo apt install docker-ce
+}
+
+read -p $'\nIs this the correct docker-ce distribution? ' docker_choice
+case "$docker_choice" in
+	y|Y ) install_dockerce;;
+	n|N ) echo "docker-ce not verified and not installed";;
+	  * ) echo "invalid input";;
+esac
+
+echo "Installing docker-compose..."
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Creating symlinks for configs..."
 for nixfile in $nixfiles; do

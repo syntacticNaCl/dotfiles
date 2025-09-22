b47d26cd1c7b6ad6e19fc79674971cda1776b877
syntacticNaCl <garrettrappaport@protonmail.ch>
2025-09-22 10:44:18 -0400

    reset keymap sequence timeout

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -51,7 +51,7 @@ vim.o.signcolumn = 'yes'
 vim.o.updatetime = 250
 
 -- Decrease mapped sequence wait time
-vim.o.timeoutlen = 300
+-- vim.o.timeoutlen = 1000
 
 -- Configure how new splits should be opened
 vim.o.splitright = true

ba36db4a5b48b385848cb60d901701a692837a98
syntacticNaCl <garrettrappaport@protonmail.ch>
2025-09-22 11:05:03 -0400

    add python3 host prog

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,8 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+vim.g.python3_host_prog = '~/.asdf/shims/python3'
+
 -- [[ Import Config ]]
 require 'config.autocmds'
 require 'config.keymaps'
